27045cfe4d47885897c7d1f47b79fa3273800416
Emily Malec Brown <emilymalecbrown@gmail.com>
2016-07-29 10:40:39 -0400

    Update bubblesort.js
    remove comments

--- bubblesort.js
+++ bubblesort.js
@@ -14,15 +14,3 @@ function bubbleSort(array) {
 }
 
 
-
-// for (var i=0; i<array.length; i++) {
-// 		for (var j=i+1; j<array.length; j++) {
-// 			if (array[i] > array[j]) {
-// 				newArray.push(array[j]);
-// 				swap++;
-// 				break;
-// 			} else {
-// 				newArray.push(array[i]);
-// 			}
-// 		} comparison++;
-// 	}
